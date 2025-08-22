ba2a631f32a15a30bfff33718a8bd62ecd8a6191
mindtravel <2200013088@stu.pku.edu.cn>
2025-08-23 00:20:44 +0800

    在sql层完整地加入ivfjl的选项

--- sql/vector.sql
+++ sql/vector.sql
@@ -254,14 +254,14 @@ CREATE ACCESS METHOD ivfflat TYPE INDEX HANDLER ivfflathandler;
 
 COMMENT ON ACCESS METHOD ivfflat IS 'ivfflat index access method';
 
--- -- ivfjl处理函数
--- CREATE FUNCTION ivfjlhandler(internal) RETURNS index_am_handler
---     AS 'MODULE_PATHNAME' LANGUAGE C;
+-- ivfjl处理函数
+CREATE FUNCTION ivfjlhandler(internal) RETURNS index_am_handler
+    AS 'MODULE_PATHNAME' LANGUAGE C;
 
--- -- ivfjl访问方法
--- CREATE ACCESS METHOD ivfjl TYPE INDEX HANDLER ivfjlhandler;
+-- ivfjl访问方法
+CREATE ACCESS METHOD ivfjl TYPE INDEX HANDLER ivfjlhandler;
 
--- COMMENT ON ACCESS METHOD ivfjl IS 'ivfjl index access method';
+COMMENT ON ACCESS METHOD ivfjl IS 'ivfjl index access method';
 
 CREATE FUNCTION hnswhandler(internal) RETURNS index_am_handler
 	AS 'MODULE_PATHNAME' LANGUAGE C;
@@ -313,6 +313,28 @@ CREATE OPERATOR CLASS vector_ip_ops
 
 CREATE OPERATOR CLASS vector_cosine_ops
 	FOR TYPE vector USING ivfflat AS
+	OPERATOR 1 <=> (vector, vector) FOR ORDER BY float_ops,
+	FUNCTION 1 vector_negative_inner_product(vector, vector),
+	FUNCTION 2 vector_norm(vector),
+	FUNCTION 3 vector_spherical_distance(vector, vector),
+	FUNCTION 4 vector_norm(vector);
+
+CREATE OPERATOR CLASS vector_l2_ops
+	DEFAULT FOR TYPE vector USING ivfjl AS
+	OPERATOR 1 <-> (vector, vector) FOR ORDER BY float_ops,
+	FUNCTION 1 vector_l2_squared_distance(vector, vector),
+	FUNCTION 3 l2_distance(vector, vector);
+
+CREATE OPERATOR CLASS vector_ip_ops
+	FOR TYPE vector USING ivfjl AS
+	OPERATOR 1 <#> (vector, vector) FOR ORDER BY float_ops,
+	FUNCTION 1 vector_negative_inner_product(vector, vector),
+	FUNCTION 3 vector_spherical_distance(vector, vector),
+	FUNCTION 4 vector_norm(vector);
+
+-- ivfjl operator classes
+CREATE OPERATOR CLASS vector_cosine_ops
+	FOR TYPE vector USING ivfjl AS
 	OPERATOR 1 <=> (vector, vector) FOR ORDER BY float_ops,
 	FUNCTION 1 vector_negative_inner_product(vector, vector),
 	FUNCTION 2 vector_norm(vector),
@@ -631,6 +653,31 @@ CREATE OPERATOR CLASS halfvec_cosine_ops
 	FUNCTION 4 l2_norm(halfvec),
 	FUNCTION 5 ivfflat_halfvec_support(internal);
 
+-- 复用ivfflat的halfvec_support
+CREATE OPERATOR CLASS halfvec_l2_ops
+	FOR TYPE halfvec USING ivfjl AS
+	OPERATOR 1 <-> (halfvec, halfvec) FOR ORDER BY float_ops,
+	FUNCTION 1 halfvec_l2_squared_distance(halfvec, halfvec),
+	FUNCTION 3 l2_distance(halfvec, halfvec),
+	FUNCTION 5 ivfflat_halfvec_support(internal);
+
+CREATE OPERATOR CLASS halfvec_ip_ops
+	FOR TYPE halfvec USING ivfjl AS
+	OPERATOR 1 <#> (halfvec, halfvec) FOR ORDER BY float_ops,
+	FUNCTION 1 halfvec_negative_inner_product(halfvec, halfvec),
+	FUNCTION 3 halfvec_spherical_distance(halfvec, halfvec),
+	FUNCTION 4 l2_norm(halfvec),
+	FUNCTION 5 ivfflat_halfvec_support(internal);
+
+CREATE OPERATOR CLASS halfvec_cosine_ops
+	FOR TYPE halfvec USING ivfjl AS
+	OPERATOR 1 <=> (halfvec, halfvec) FOR ORDER BY float_ops,
+	FUNCTION 1 halfvec_negative_inner_product(halfvec, halfvec),
+	FUNCTION 2 l2_norm(halfvec),
+	FUNCTION 3 halfvec_spherical_distance(halfvec, halfvec),
+	FUNCTION 4 l2_norm(halfvec),
+	FUNCTION 5 ivfflat_halfvec_support(internal);
+
 CREATE OPERATOR CLASS halfvec_l2_ops
 	FOR TYPE halfvec USING hnsw AS
 	OPERATOR 1 <-> (halfvec, halfvec) FOR ORDER BY float_ops,
@@ -680,6 +727,14 @@ CREATE OPERATOR <%> (
 
 CREATE OPERATOR CLASS bit_hamming_ops
 	FOR TYPE bit USING ivfflat AS
+	OPERATOR 1 <~> (bit, bit) FOR ORDER BY float_ops,
+	FUNCTION 1 hamming_distance(bit, bit),
+	FUNCTION 3 hamming_distance(bit, bit),
+	FUNCTION 5 ivfflat_bit_support(internal);
+
+-- 复用ivfflat的bit_support
+CREATE OPERATOR CLASS bit_hamming_ops
+	FOR TYPE bit USING ivfjl AS
 	OPERATOR 1 <~> (bit, bit) FOR ORDER BY float_ops,
 	FUNCTION 1 hamming_distance(bit, bit),
 	FUNCTION 3 hamming_distance(bit, bit),
